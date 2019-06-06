9624aee813f27fb42aceedb83bd4b5c10abe63ba
Reyaad Rafeek <reyaad510@gmail.com>
2019-06-05 22:12:04 -0400

    mvp completed

--- index.js
+++ index.js
@@ -16,7 +16,87 @@ server.get('/', (req, res) => {
   res.send("Is this thing working?");
 });
 
+// Register
 
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+  
+    if (!user.username || !user.password) {
+      return res.status(500).json({ message: "Need name and pass" })
+    }
+  
+    if (user.password.length < 6) {
+      return res.status(400).json({ message: 'Pass too short!' })
+    }
+  
+    const hash = bcrypt.hashSync(user.password, 14);
+    user.password = hash;
+  
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  
+  // LOGIN
+  server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+  
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if(!username || !password) {
+          return res.status(401).json({ message: 'Invalid Credentials' })
+        }
+  
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'You shall not pass!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  
+  // MIDDLE WARE
+  function authorize(req, res, next) {
+    const username = req.headers['x-username'];
+    const password = req.headers['x-password'];
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+  
+        if(!username || !password) {
+          return res.status(401).json({ message: 'Invalid Credentials' })
+        }
+  
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next()
+        } else {
+          res.status(401).json({ message: 'You shall not pass!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  }
+  
+  server.get('/api/users', authorize, (req, res) => {
+    Users.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
 
 
 
